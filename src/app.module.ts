7cd274b62da4a3c83a207a5c6a1f6259d3b57b2e
Gabrielli <gabivirgilio@oultook.com>
2024-08-09 10:52:45 -0300

    CRUD Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [
@@ -12,11 +12,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
 
-    })
-
+    }),
+    CategoriaModule
   ],
   controllers: [],
   providers: [],
